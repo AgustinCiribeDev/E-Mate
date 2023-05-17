0a425fba21adb4eaa91ed2825f3e1a80f4576fc5
Santiago Marconetti <smarconetti@gmail.com>
2023-05-17 09:25:53 -0300

    Actualización del proyecto con carpetas public y views

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require ('express');
+const path = requiere ('path');
+const app = express ();
+
+app.use (express.static (path.join (__dirname, '/public')));
+
+app.listen (3002, function () {
+    console.log ("Servidor corriendo");
+});
