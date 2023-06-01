9e034af696f985f50bcb7b4c7b3927bc5b743015
Santiago Marconetti <smarconetti@gmail.com>
2023-06-01 20:39:47 -0300

    Se actualiza app.js y se agregan htmls

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require ('express');
-const path = requiere ('path');
+const path = require ('path');
 const app = express ();
 
 app.use (express.static (path.join (__dirname, '/public')));
+
+app.get ('/', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/index.html'));
+});
 
 app.listen (3002, function () {
     console.log ("Servidor corriendo");
