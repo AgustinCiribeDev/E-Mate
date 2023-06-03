021f28e7a0782c19173651af243a935ee6d587e8
EzeAllende <ezequielallende@gmail.com>
2023-06-03 15:52:04 -0300

    Agregando libreria Bootstrap

--- app.js
+++ app.js
@@ -2,11 +2,29 @@ const express = require ('express');
 const path = require ('path');
 const app = express ();
 
-app.use (express.static (path.join (__dirname, '/public')));
+app.use (express.static (path.resolve (__dirname, '/public')));
 
 app.get ('/', (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/index.html'));
 });
+
+app.get ('/register', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/register.html'));
+});
+
+app.get ('/login', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/login.html'));
+});
+
+app.get ('/producto', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/productDetail.html'));
+});
+
+app.get ('/carrito', (req, res) => {
+    res.sendFile (path.resolve (__dirname, './views/productCart.html'));
+});
+
+
 
 app.listen (3002, function () {
     console.log ("Servidor corriendo");
