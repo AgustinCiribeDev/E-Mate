7b76768f363e3465f565d73e5506d24702ea5dc6
EzeAllende <ezequielallende@gmail.com>
2023-06-12 20:16:59 -0300

    probando algo nuevo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ('express');
 const path = require ('path');
 const app = express ();
 
-app.use (express.static (path.resolve (__dirname, '/public')));
+app.use (express.static (path.resolve (__dirname, './public')));
 
 app.get ('/', (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/index.html'));
