c9827d649edd15e1ab57a7a582dbb38b092660e2
AgustinCiribeDev <agu.ciribe@gmail.com>
2023-06-15 13:34:49 -0300

    cambios en el header

--- app.js
+++ app.js
@@ -31,3 +31,25 @@ app.get ('/carrito', (req, res) => {
 app.listen (3002, function () {
     console.log ("Servidor corriendo en puerto 3002");
 });
+
+$(document).ready(function() {
+    $("#sidebarCollapse").on("click", function() {
+      $("#sidebar").addClass("active");
+    });
+  
+    $("#sidebarCollapseX").on("click", function() {
+      $("#sidebar").removeClass("active");
+    });
+  
+    $("#sidebarCollapse").on("click", function() {
+      if ($("#sidebar").hasClass("active")) {
+        $(".overlay").addClass("visible");
+        console.log("it's working!");
+      }
+    });
+  
+    $("#sidebarCollapseX").on("click", function() {
+      $(".overlay").removeClass("visible");
+    });
+  });
+  
