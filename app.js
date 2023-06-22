1d25999fe6f20f201e8bbd9e2952264d46a86054
Santiago Marconetti <smarconetti@gmail.com>
2023-06-22 18:07:43 -0300

    Se agregan notas en el app.js para revisar con el equipo para sprint 3

--- app.js
+++ app.js
@@ -7,47 +7,48 @@ app.set("view engine", "ejs");
 
 // app.set('views',__dirname + '/views') // Se comenta código porque es posible que no sea necesario
 
-app.use('/css', express.static(path.resolve (__dirname, "node_modules/bootstrap/dist/css")))
+app.use('/css', express.static(path.resolve (__dirname, "node_modules/bootstrap/dist/css"))); // Este va
 
-app.use('/js', express.static(path.resolve (__dirname, "node_modules/bootstrap/dist/js")))
+app.use('/js', express.static(path.resolve (__dirname, "node_modules/bootstrap/dist/js"))); // Este va
 
-app.use('/icons', express.static(path.resolve (__dirname, "node_modules/bootstrap-icons/font")))
+app.use('/icons', express.static(path.resolve (__dirname, "node_modules/bootstrap-icons/font"))); // Este va
 
-app.use (express.static (path.resolve (__dirname, './public')));
+app.use (express.static (path.resolve (__dirname, './public'))); // Este va
 
 app.get('/',(req, res) => { 
     res.render('index')
     
-});
+}); // Este a routes y controllers
 
 app.get ('/header', (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/header'));
-});
+}); // Este se va y se incluye con include en la sección que se necesite
 
 app.get ('/footer', (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/footer'));
-});
+}); // Este se va y se incluye con include en la sección que se necesite
 
 app.get ('/register', (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/register.html'));
-});
+}); // Este a routes y controllers
 
 app.get ('/login', (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/login.html'));
-});
+});  // Este a routes y controllers
 
 app.get ('/producto', (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/productDetail.html'));
-});
+}); // Este a routes y controllers
 
 app.get ('/carrito', (req, res) => {
     res.sendFile (path.resolve (__dirname, './views/productCart.html'));
-});
+}); // Este a routes y controllers
 
 
 app.listen (port, function () {
     console.log ("Servidor corriendo en puerto " + port);
-});
+}); // Este va
+
 
 
 
