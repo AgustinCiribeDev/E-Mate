9491f86e47c5d8540d6161590cfb2e6e85eab3d8
rociokite <rociokite@gmail.com>
2023-07-20 00:13:41 -0300

    Carga de imagenes ok

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -54,36 +54,38 @@ const productsControllers = {
           res.render('products/productCreate');
         },
         addProduct: (req, res) => {
-		      const imageBuffer = req.file.buffer;
+          const imageBuffer = req.file.buffer;
           const nombreImagen = Date.now() + req.file.originalname;
-
-          const stream = cloudinary.uploader.upload_stream({resource_type: 'image', public_id: nombreImagen}, (error, result) => {
-             if (error) {
-               console.log('Error al cargar la imagen:', error);
-             } else {
-               console.log('Imagen cargada correctamente:', result);
-               }
-           });
-           streamifier.createReadStream(imageBuffer).pipe(stream);
-          
-          let idNuevoProducto= (productos[productos.length-1].id)+1;
-          let objNuevoProducto= {
-            id: idNuevoProducto,
-            nombre: req.body.nombre,
-            precioActual: parseInt(req.body.precioActual),
-            categoria: req.body.categoria,
-            descripcion: req.body.descripcion,
-            cuotas: parseInt(req.body.cuotas),
-            estado: req.body.estado,
-            descuento: parseInt(req.body.descuento),
-            imagen: nombreImagen
-          };
         
-          productos.push(objNuevoProducto);
-          fs.writeFileSync(productsFilePath, JSON.stringify(productos, null, ' '));
-          
-          res.redirect('/');
+          const stream = cloudinary.uploader.upload_stream({ resource_type: 'image', public_id: nombreImagen }, (error, result) => {
+            if (error) {
+              console.log('Error al cargar la imagen:', error);
+              res.status(500).send('Error al cargar la imagen');
+            } else {
+              console.log('Imagen cargada correctamente:', result);
+              // Aquí, en lugar de almacenar solo el nombre de la imagen,
+              // almacenamos la URL completa de Cloudinary en el objeto del nuevo producto
+              let idNuevoProducto = (productos[productos.length - 1].id) + 1;
+              let objNuevoProducto = {
+                id: idNuevoProducto,
+                nombre: req.body.nombre,
+                precioActual: parseInt(req.body.precioActual),
+                categoria: req.body.categoria,
+                descripcion: req.body.descripcion,
+                cuotas: parseInt(req.body.cuotas),
+                estado: req.body.estado,
+                descuento: parseInt(req.body.descuento),
+                imagen: result ? result.secure_url : null // Almacenamos la URL completa de Cloudinary si result está definido, de lo contrario, usamos null
+              };
         
+              productos.push(objNuevoProducto);
+              fs.writeFileSync(productsFilePath, JSON.stringify(productos, null, ' '));
+        
+              res.redirect('/');
+            }
+          });
+        
+          streamifier.createReadStream(imageBuffer).pipe(stream);
         },
         // Buscar producto a editar
         edit: (req,res) => {
