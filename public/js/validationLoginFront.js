a0c0b735c90d99cfcb473d04a86717854ccd4fd3
EzeAllende <ezequielallende@gmail.com>
2023-09-23 12:54:17 -0300

    subiendo validacion front del login

---
+++ public/js/validationLoginFront.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", function(){
+
+let formLogin = document.querySelector('form');
+
+formLogin.addEventListener('submit', function (evento){
+    let errores = [];
+
+    let campoEmail = document.querySelector('#email');
+
+    let campoPassword = document.querySelector('#password');
+
+    if(campoEmail.value == ""){
+        errores.push('El campo Email está vacío');
+    }
+    if(campoPassword.value == ""){
+        errores.push('No escribiste tu contraseña');
+    }
+console.log(errores)
+    if(errores.length > 0){
+        evento.preventDefault();
+        let ulErrores = document.querySelector('div.errores ul');
+
+        for(let i =0; i< errores.length; i++){
+            ulErrores.innerHTML += "<li>" + errores[i] + "</li>"
+        }
+    }
+
+})
+
+})
